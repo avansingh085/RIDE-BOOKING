a4c68ddc71c801d6b041557b8b02766a4f01e498
Avan <avansingh085@gmail.com>
2025-05-28 15:04:22 +0530

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,7 +4,7 @@ import { getUserById, updateUser, deleteUser } from '../controllers/userControll
 import verifyToken from '../middilewares/auth/verifyToken.js';
 import { verifyAdmin } from '../middilewares/auth/verifyAdmin.js';
 const router = express.Router();
-router.get('/',verifyToken,verifyAdmin, getUserById);
-router.put('/',verifyToken,verifyAdmin, updateUser);
+router.get('/',verifyToken, getUserById);
+router.put('/',verifyToken, updateUser);
 router.delete('/',verifyToken, deleteUser);
 export default router;
